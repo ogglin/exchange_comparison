6515531414918f30cabc9ce499bc05f22503ab1f
ares <server.ares@gmail.com>
2020-08-13 11:46:20 +0300

    v0.0.2 wsgi fix and requirements update

--- exchange_comparison/wsgi.py
+++ exchange_comparison/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/exchange_comparison')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exchange_comparison.settings')
 
 application = get_wsgi_application()
