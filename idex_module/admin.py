7bc4735d3d830733a00c824c5bb6ea889955e6a7
ares <server.ares@gmail.com>
2020-08-24 12:17:47 +0300

    v0.0.4a add setting

--- idex_module/admin.py
+++ idex_module/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Idex)
+@admin.register(Idex)
+class IdexAdmin(admin.ModelAdmin):
+    list_display = ('exch_direction', 'is_active')
--- idex_module/models.py
+++ idex_module/models.py
@@ -8,6 +8,7 @@ class Idex(models.Model):
     exch_direction = models.CharField(max_length=20, blank=False, null=False)
     lowest_ask = models.FloatField()
     highest_bid = models.FloatField()
+    is_active = models.BooleanField(default=True, verbose_name='Активный')
 
     def __str__(self):
         return self.exch_direction
