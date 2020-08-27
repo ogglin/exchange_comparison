9f0fd944fd19c3b90954386a7e4b2be9b48fef14
ares <server.ares@gmail.com>
2020-08-27 13:49:17 +0300

    v0.1.1a fix Models length

--- uniswap_module/admin.py
+++ uniswap_module/admin.py
@@ -4,5 +4,10 @@ from .models import *
 
 # Register your models here.
 @admin.register(Uniswap)
-class BancorAdmin(admin.ModelAdmin):
+class UniswapAdmin(admin.ModelAdmin):
+    list_display = ('exch_direction', 'is_active')
+
+
+@admin.register(UniswapOne)
+class UniswapOneAdmin(admin.ModelAdmin):
     list_display = ('exch_direction', 'is_active')
