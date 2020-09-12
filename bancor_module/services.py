b8db6c3710bc5522c42a25d46d985ea59eaae7e2
ares <server.ares@gmail.com>
2020-09-12 21:28:57 +0300

    v0.2.1d fix bancor None price

--- bancor_module/services.py
+++ bancor_module/services.py
@@ -20,17 +20,20 @@ def currencies_update(direction, lowest_ask, highest_bid, name, link_id):
 
 def set_currencies():
     proxies = {
-        'http': '46.102.73.244:53281',
-        'https': '199.91.203.210:3128'
+        'http': '87.76.35.86:35782',
+        'https': '162.144.37.206:3838'
     }
     url = 'https://api.bancor.network/0.1/currencies/tokens?limit=9999&skip=0&fromCurrencyCode=ETH&includeTotal=true&orderBy=code&sortOrder=asc&skip=0'
     # response = requests.get(url=url, proxies=proxies)
     response = requests.get(url=url)
     jData = json.loads(response.content)['data']['page']
     for data in jData:
-        direction = data['code']
-        highest_bid = data['price'] * koef
-        lowest_ask = data['price']
-        name = data['name']
-        link_id = data['id']
-        currencies_update(direction, lowest_ask, highest_bid, name, link_id)
+        if data['price'] is not None:
+            direction = data['code']
+            highest_bid = data['price'] * koef
+            lowest_ask = data['price']
+            name = data['name']
+            link_id = data['id']
+            currencies_update(direction, lowest_ask, highest_bid, name, link_id)
+
+
