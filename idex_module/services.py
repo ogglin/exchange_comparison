4f580419e9391b71869d96c094172866ae38c61f
ares <server.ares@gmail.com>
2020-10-21 16:07:09 +0300

    v0.2.4 rework idex and pairs

--- idex_module/services.py
+++ idex_module/services.py
@@ -2,25 +2,27 @@ import json
 
 import requests
 
-from .models import *
+# from .models import *
 
 
 def currencies_update(token_pair, ask, bid):
-    pair_id = Idex.objects.filter(exch_direction=token_pair).values('id')
-    if len(pair_id) > 0:
-        Idex.objects.filter(id=pair_id[0]['id']).update(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid)
-    else:
-        pair = Idex(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid, is_active=True)
-        pair.save()
+    print(token_pair, ask, bid)
+    # pair_id = Idex.objects.filter(exch_direction=token_pair).values('id')
+    # if len(pair_id) > 0:
+    #     Idex.objects.filter(id=pair_id[0]['id']).update(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid)
+    # else:
+    #     pair = Idex(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid, is_active=True)
+    #     pair.save()
 
 
 def set_currencies():
-    ask = 0
-    bid = 0
     url = 'https://api.idex.io/v1/tickers'
     response = requests.get(url=url)
     jData = json.loads(response.content)
     for data in jData:
+        ask = 0
+        bid = 0
+        print(data, type(data['ask']), data['ask'], type(data['bid']), data['bid'], data['market'])
         token_pair = data['market'].replace('ETH', '').replace('-', '')
         if data['ask']:
             ask = data['ask']
@@ -28,3 +30,4 @@ def set_currencies():
             bid = data['bid']
         currencies_update(token_pair, ask, bid)
 
+set_currencies()
