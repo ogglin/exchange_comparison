37742c001bba767c63def9abeb831ad5098f181d
ares <server.ares@gmail.com>
2020-10-29 15:46:12 +0300

    v0.2.5h fix celery

--- idex_module/tasks.py
+++ idex_module/tasks.py
@@ -1,20 +1,9 @@
 import sys
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from celery.task import periodic_task
-from exchange_comparison._celery import app
+
 from .services import set_currencies
-from .socket_services import get_wss
-
-
-@app.task(bind=True, task_acks_late=True, worker_prefetch_multiplier=1)
-def websock(*args, **kwargs):
-    print('Try Idex websocket connect ' + str(datetime.utcnow() + timedelta(hours=3)))
-    get_wss()
-
-
-started_at = datetime.utcnow() + timedelta(hours=3, seconds=30)
-websock.apply_async((), countdown=30)
 
 
 @periodic_task(run_every=(timedelta(seconds=5)), queue='normal',
