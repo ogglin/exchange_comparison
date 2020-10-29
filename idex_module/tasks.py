971775890b212072f99636850746ec4984290665
ares <server.ares@gmail.com>
2020-10-29 15:32:48 +0300

    v0.2.5h fix celery

--- idex_module/tasks.py
+++ idex_module/tasks.py
@@ -7,7 +7,7 @@ from .services import set_currencies
 from .socket_services import get_wss
 
 
-@app.task(bind=True)
+@app.task(bind=True, task_acks_late=True, worker_prefetch_multiplier=1)
 def websock(*args, **kwargs):
     print('Try Idex websocket connect ' + str(datetime.utcnow() + timedelta(hours=3)))
     get_wss()
