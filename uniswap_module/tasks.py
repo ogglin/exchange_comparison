5cdf34bf89a33cd0f4420db94c75c804d82f55da
ares <server.ares@gmail.com>
2020-10-29 20:10:18 +0300

    v0.2.5h fix celery

--- uniswap_module/tasks.py
+++ uniswap_module/tasks.py
@@ -11,7 +11,9 @@ from .services import set_all_currencies
 # @app.task()
 def uniswap_currencies_update():
     try:
+        print('Uniswap collect data try')
         set_all_currencies()
+        print('Uniswap data collected')
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
