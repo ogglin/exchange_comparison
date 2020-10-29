5b4bc4af15f8dd01f02c4653fe69d904dcc26c8d
ares <server.ares@gmail.com>
2020-10-29 14:19:35 +0300

    v0.2.5h fix celery

--- idex_module/tasks.py
+++ idex_module/tasks.py
@@ -11,10 +11,13 @@ from .socket_services import get_wss
 def websock(self):
     status = websock.AsyncResult(websock.request.id).state
     print(status)
-    self.update_state(state='PROGRESS')
-    print(websock.AsyncResult(websock.request.id).state)
-    print('Try Idex websocket connect ' + str(datetime.now() + timedelta(seconds=10)))
-    get_wss()
+    if websock.AsyncResult(websock.request.id).state == 'PROGRESS':
+        print('Status: ' + status)
+    else:
+        websock.update_state(state='PROGRESS')
+        print(websock.AsyncResult(websock.request.id).state)
+        print('Try Idex websocket connect ' + str(datetime.now() + timedelta(seconds=10)))
+        get_wss()
 
 
 # started_at = datetime.utcnow() + timedelta(hours=3, seconds=10)
