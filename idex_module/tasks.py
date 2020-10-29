a94976135c834da83186c13a9fafcd75bd50df04
ares <server.ares@gmail.com>
2020-10-29 14:47:38 +0300

    v0.2.5h fix celery

--- idex_module/tasks.py
+++ idex_module/tasks.py
@@ -6,24 +6,15 @@ from exchange_comparison._celery import app
 from .services import set_currencies
 from .socket_services import get_wss
 
-isStart = False
 
 @app.task(bind=True)
 def websock(self):
-    global isStart
-    status = websock.AsyncResult(websock.request.id).state
-    print(status, isStart)
-    if websock.AsyncResult(websock.request.id).state == 'STARTED' and isStart:
-        print('Status: ' + status)
-    else:
-        print(websock.AsyncResult(websock.request.id).state)
-        print('Try Idex websocket connect ' + str(datetime.now() + timedelta(seconds=10)))
-        isStart = True
-        get_wss()
+    print('Try Idex websocket connect ' + str(datetime.now() + timedelta(seconds=10)))
+    get_wss()
 
 
-# started_at = datetime.utcnow() + timedelta(hours=3, seconds=10)
-# websock.apply_async(eta=started_at)
+started_at = datetime.utcnow() + timedelta(hours=3, seconds=10)
+websock.apply_async(eta=started_at)
 
 
 @periodic_task(run_every=(timedelta(seconds=5)), queue='normal',
