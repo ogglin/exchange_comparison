98efe48be8539a13fc113f03ad7d6780a289d06a
ares <server.ares@gmail.com>
2020-10-29 15:28:40 +0300

    v0.2.5h fix celery

--- idex_module/tasks.py
+++ idex_module/tasks.py
@@ -14,7 +14,7 @@ def websock(*args, **kwargs):
 
 
 started_at = datetime.utcnow() + timedelta(hours=3, seconds=30)
-websock.apply_async((), eta=started_at)
+websock.apply_async((), countdown=30)
 
 
 @periodic_task(run_every=(timedelta(seconds=5)), queue='normal',
