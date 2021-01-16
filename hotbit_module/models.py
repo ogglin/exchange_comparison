7c2c95a3f727cff334394384772490c2cc507554
ares <server.ares@gmail.com>
2021-01-16 16:02:02 +0300

    v0.2.8b add hotbit module fix

--- hotbit_module/models.py
+++ hotbit_module/models.py
@@ -4,10 +4,11 @@ from django.db import models
 # Create your models here.
 
 class Hotbit(models.Model):
-    exch_direction = models.CharField(max_length=20, blank=False, null=False)
+    exch_direction = models.CharField(max_length=30, blank=False, null=False)
     buy = models.FloatField()
     sell = models.FloatField()
-    symbol = models.CharField(max_length=20, blank=False, null=False)
+    symbol = models.CharField(max_length=30, blank=False, null=False)
+    volume = models.FloatField(max_length=30, blank=False, null=False)
     is_active = models.BooleanField(verbose_name='Активный', blank=False, null=False)
 
     def __str__(self):
