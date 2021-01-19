59dca79a0ad880c7c5a3f9405a35219cac556e8b
ares <server.ares@gmail.com>
2021-01-19 15:13:13 +0300

    v0.2.8c add api fix

--- hotbit_module/views.py
+++ hotbit_module/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+import requests
+import json
 
-from exchange_comparison.services import token_exchange_set
-from .functions import set_currencies
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
-def hotbit(request):
-    # set_currencies()
-    token_exchange_set()
-    return render(request)
+
+class hotbit(APIView):
+    # token_exchange_set()
+
+    def get(self, request):
+        if request.GET['symbol']:
+            url = "https://api.hotbit.io/api/v1/order.depth?interval=1e-8&&limit=20&market="+request.GET['symbol']
+        else:
+            url = "https://api.hotbit.io/api/v1/order.depth?interval=1e-8&&limit=20&market="
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return Response(json.loads(response.text))
