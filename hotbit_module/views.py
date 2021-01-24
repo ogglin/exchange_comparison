6c05c56059b0e34b2e6c8ee6652a16d1a83984f9
ares <server.ares@gmail.com>
2021-01-24 15:47:52 +0300

    v0.2.8c add api fix

--- hotbit_module/views.py
+++ hotbit_module/views.py
@@ -33,18 +33,6 @@ PRODUCERS_COUNT = 20
 #     'http://fly.paidoohost.com/',
 # ]
 
-'''
-185.36.189.232
-193.111.152.90
-
-
-
-
-
-
-
-
-'''
 
 proxys = [
     ['185.36.189.232', '16714', 'user53105', '3x7cyr'],
@@ -60,9 +48,24 @@ proxys = [
 ]
 
 
+idex_tiker_all = None
+
+
+async def get_idex_tiker_all():
+    url = f'https://api.idex.io/v1/tickers'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            return json.loads(html)
+
+
 async def get_idex_market(token, proxy):
-    url = f'https://api.idex.io/v1/tickers?market={token}-ETH'
-    print(url + str(datetime.datetime.now()), proxy)
+    for tiker in idex_tiker_all:
+        if tiker['market'].replace('-ETH', '') == token:
+            return tiker
+    return None
+    # url = f'https://api.idex.io/v1/tickers?market={token}-ETH'
+    # print(url + str(datetime.datetime.now()), proxy)
     # socks_url = 'http://' + prx[2] + ':' + prx[3] + '@' + prx[1] + ':' + prx[0]
     # print(url + str(datetime.datetime.now()), socks_url)
     # reader, writer = await open_connection(
@@ -78,19 +81,19 @@ async def get_idex_market(token, proxy):
     # writer.write(request)
     # print(reader.read(-1))
     # return await json.loads(reader.read(-1))
-    connector = ProxyConnector(
-        proxy_type=ProxyType.SOCKS5,
-        host=proxy[0],
-        port=16714,
-        username='user53105',
-        password='3x7cyr',
-        rdns=True
-    )
-
-    async with aiohttp.ClientSession(connector=connector) as session:
-        async with session.get(url) as response:
-            html = await response.text()
-            return json.loads(html)
+    # connector = ProxyConnector(
+    #     proxy_type=ProxyType.SOCKS5,
+    #     host=proxy[0],
+    #     port=16714,
+    #     username='user53105',
+    #     password='3x7cyr',
+    #     rdns=True
+    # )
+    #
+    # async with aiohttp.ClientSession(connector=connector) as session:
+    #     async with session.get(url) as response:
+    #         html = await response.text()
+    #         return json.loads(html)
 
 
 async def get_hotbit_depth(symbol, proxy):
@@ -119,7 +122,7 @@ async def compare(asks, bids, where, to, symbols, percent):
 
     if type(asks) is float:
         for bid in bids:
-            if (float(bid[0]) * percent / 100 + float(bid[0])) > asks:
+            if float(bid[0]) > (asks * percent / 100 + asks):
                 full_price += float(bid[0])
                 volume += float(bid[1])
                 ask_price = asks
@@ -127,18 +130,18 @@ async def compare(asks, bids, where, to, symbols, percent):
 
     if type(bids) is float:
         for ask in asks:
-            if (float(ask[0]) + float(ask[0]) * percent / 100) > bids:
+            if (float(ask[0]) + float(ask[0]) * percent / 100) < bids:
                 full_price += float(ask[0])
                 volume += float(ask[1])
                 bid_price = bids
         ask_price = full_price / count
 
-    if ask_price > 0 and bid_price > 0 and volume > 0:
+    if bid_price > ask_price > 0 and volume > 0:
         # print('/--------------------------')
         # print('/ ' + w_symbol + ' from ' + where + ' to ' + t_symbol + ' ' + to + ' /')
         # print('/ ask ' + str(ask_price) + ' bid ' + str(bid_price) + ' volume ' + str(volume) + ' /')
         # print('--------------------------/')
-        return [w_symbol, where, t_symbol, to, ask_price, bid_price, volume]
+        return [w_symbol, where, ask_price, t_symbol, to, bid_price, volume, (bid_price-ask_price)/bid_price*100]
     else:
         return None
 
@@ -147,36 +150,35 @@ async def compare_markets(symbol, percent, proxy):
     compares = []
     idex_ticker = await get_idex_market(symbol[0], proxy)
     hotbit_depth = await get_hotbit_depth(symbol[1], proxy)
-    if hotbit_depth['error'] is None:
-        try:
-            print('idex', proxy[0], idex_ticker['code'])
-        except:
-            if idex_ticker[0]['ask']:
-                idex_ask = float(idex_ticker[0]['ask'])
-            else:
-                idex_ask = None
-            if idex_ticker[0]['bid']:
-                idex_bid = float(idex_ticker[0]['bid'])
-            else:
-                idex_bid = None
-            hitbit_asks = hotbit_depth['result']['asks']
-            hitbit_bids = hotbit_depth['result']['bids']
-            a = await compare(idex_ask, hitbit_bids, 'idex', 'hotbit', symbol, percent)
-            if a is not None:
-                compares.append(a)
-            b = await compare(hitbit_asks, idex_bid, 'hotbit', 'idex', symbol, percent)
-            if b is not None:
-                compares.append(b)
+    if hotbit_depth['error'] is None and idex_ticker is not None:
+        if idex_ticker['ask']:
+            idex_ask = float(idex_ticker['ask'])
+        else:
+            idex_ask = None
+        if idex_ticker['bid']:
+            idex_bid = float(idex_ticker['bid'])
+        else:
+            idex_bid = None
+        hotbit_asks = hotbit_depth['result']['asks']
+        hotbit_bids = hotbit_depth['result']['bids']
+        a = await compare(idex_ask, hotbit_bids, 'idex', 'hotbit', symbol, percent)
+        if a is not None:
+            compares.append(a)
+        b = await compare(hotbit_asks, idex_bid, 'hotbit', 'idex', symbol, percent)
+        if b is not None:
+            compares.append(b)
     return compares
 
 
 async def init(symbols, percent):
+    global idex_tiker_all
+    idex_tiker_all = await get_idex_tiker_all()
     async_tasks = []
-    print('start collect symbols ' + str(len(symbols)) + ' :' + str(datetime.datetime.now()))
+    # print('start collect symbols ' + str(len(symbols)) + ' :' + str(datetime.datetime.now()))
     for symbol in symbols:
         rn = randrange(len(proxys))
         async_tasks.append(compare_markets(symbol, percent, proxys[rn]))
-    print('end  collect symbols: ' + str(len(async_tasks)) + str(datetime.datetime.now()))
+    # print('end  collect symbols: ' + str(len(async_tasks)) + str(datetime.datetime.now()))
     results = await asyncio.gather(*async_tasks)
     return results
 
@@ -185,7 +187,7 @@ class hotbit(APIView):
     # token_exchange_set()
 
     def get(self, request):
-        print('start: ' + str(datetime.datetime.now()))
+        # print('start: ' + str(datetime.datetime.now()))
         if request.GET['symbol']:
             url = "https://api.hotbit.io/api/v1/order.depth?interval=1e-8&&limit=20&market=" + request.GET['symbol']
         else:
@@ -193,11 +195,11 @@ class hotbit(APIView):
         payload = {}
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
-        print('end: ' + str(datetime.datetime.now()))
+        # print('end: ' + str(datetime.datetime.now()))
         return Response(json.loads(response.text))
 
     def post(self, request):
-        print('start: ' + str(datetime.datetime.now()))
+        # print('start: ' + str(datetime.datetime.now()))
         setting = Settings.objects.all()[0]
         percent = setting.market_percent / 100 * setting.market_koef
         symbols = _query(f'SELECT mi.exch_direction, mh.symbol, mh.decimals FROM exchange_pairs '
@@ -209,7 +211,11 @@ class hotbit(APIView):
         loop = asyncio.get_event_loop()
         loop.set_default_executor(concurrent.futures.ThreadPoolExecutor(max_workers=20))
         init_result = loop.run_until_complete(init(symbols, percent))
-        print(init_result)
+        compare_result = []
+        for result in init_result:
+            if len(result) > 0:
+                compare_result.append(result[0])
+        # print(json.dumps(compare_result))
         loop.close()
-        print('end: ' + str(datetime.datetime.now()))
-        return Response(symbols)
+        # print('end: ' + str(datetime.datetime.now()))
+        return Response(compare_result)
