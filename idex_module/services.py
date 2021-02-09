a159e8961f1d2865001d38f31f3c42ca2f747a94
ares <server.ares@gmail.com>
2021-02-09 16:01:56 +0300

    v0.2.10b rework idex

--- idex_module/services.py
+++ idex_module/services.py
@@ -1,24 +1,28 @@
+import datetime
 import json
 
 import requests
+
 
 from .models import *
 
 
 def currencies_update(token_pair, ask, bid, volume):
+    # print(token_pair, ask, bid, volume)
     pair_id = Idex.objects.filter(exch_direction=token_pair).values('id')
     if len(pair_id) > 0:
-        Idex.objects.filter(id=pair_id[0]['id']).update(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid,
-                                                        volume=volume)
+        Idex.objects.filter(id=pair_id[0]['id']).update(lowest_ask=ask, highest_bid=bid, volume=volume)
     else:
         pair = Idex(exch_direction=token_pair, lowest_ask=ask, highest_bid=bid, is_active=True, volume=volume)
         pair.save()
 
 
 def set_currencies():
+    # print('start idex: ' + str(datetime.datetime.now()))
     url = 'https://api.idex.io/v1/tickers'
     response = requests.get(url=url)
     jData = json.loads(response.content)
+    Idex.objects.all().update(volime=0)
     for data in jData:
         ask = 0
         bid = 0
@@ -27,5 +31,10 @@ def set_currencies():
             ask = data['ask']
         if data['bid']:
             bid = data['bid']
-        volume = data['quoteVolume']
-        currencies_update(token_pair, ask, bid, volume)
+        volume = float(data['quoteVolume'])
+        if volume > 0:
+            currencies_update(token_pair, ask, bid, volume)
+    # print('end idex: ' + str(datetime.datetime.now()))
+
+
+set_currencies()
