89a09962d76b0b14f0319aa7aea82543eaf9fd56
ares <server.ares@gmail.com>
2021-02-10 19:47:16 +0300

    v0.2.10b rework queue

---
+++ start.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+ps auxww | grep 'celery' | awk '{print $2}' | xargs kill -9
+sleep 3
+celery -A exchange_comparison purge --force
+sleep 3
+celery -A exchange_comparison worker -Q hotbit -B -l WARNING -n hotbit@vp4.ru &
+celery -A exchange_comparison worker -Q idex -B -l WARNING -n idex@vp4.ru &
+celery -A exchange_comparison worker -Q uniswap -B -l WARNING -n uniswap@vp4.ru &
+celery -A exchange_comparison worker -Q uniswap_one -B -l WARNING -n uniswap_one@vp4.ru &
+celery -A exchange_comparison worker -Q bancor -B -l WARNING -n bancor@vp4.ru &
+celery -A exchange_comparison worker -Q kyber -B -l WARNING -n kyber@vp4.ru &
+celery -A exchange_comparison worker -Q normal -B -l WARNING -n normal@vp4.ru &
