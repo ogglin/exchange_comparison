3b1aa60ab21b4bed492de55b0cc19226eb0bfb30
ares <server.ares@gmail.com>
2021-03-23 12:27:28 +0300

    v0.2.22 major fix

--- exchange_pairs/services.py
+++ exchange_pairs/services.py
@@ -9,7 +9,7 @@ async def set_all_compared_tokens():
     global all_compared_tokens
     global uniswap_prices_set
     uniswap_prices_set = await get_uni_2()
-    all_compared_tokens = gt(module='idex', _all=False).tokens()
+    all_compared_tokens = gt(module='all', _all=True).tokens()
     all_compared_tokens.extend(uniswap_prices_set)
     if len(all_compared_tokens) > 0:
         print('all_compared_tokens', len(all_compared_tokens))
