4a386f548aba9021c5ca089844552577d737e413
ares <server.ares@gmail.com>
2021-04-07 23:06:05 +0300

    2.2.2 fix

--- utils/main.py
+++ utils/main.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 from asgiref.sync import sync_to_async
 
-from utils.markets import set_hotbit_market, set_hitbtc_market, set_idex_market
-from utils.parse_etherscan import set_new_token
+# from utils.markets import set_hotbit_market, set_hitbtc_market, set_idex_market
+# from utils.parse_etherscan import set_new_token
 from utils.parse_files import get_file
 
 TEMP_DIR = str(Path(__file__).resolve(strict=True).parent.parent) + '/temp/'
@@ -13,11 +13,12 @@ file = TEMP_DIR + 'ХОТ бит контракт 07 04.txt'
 
 @sync_to_async
 def init_utest():
-    get_file(file)
+    # get_file(file)
     # set_new_token()
     # set_hotbit_market()
     # set_hitbtc_market()
     # set_idex_market()
+    pass
 
 
 if __name__ == '__main__':
