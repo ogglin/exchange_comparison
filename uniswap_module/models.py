2aad6d25d9d4a3baaac489bf5a452de5d553f8c2
ares <server.ares@gmail.com>
2021-04-22 18:47:09 +0300

    2.3.1b fix frontend

--- uniswap_module/models.py
+++ uniswap_module/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Uniswap(models.Model):
     exch_direction = models.CharField(max_length=256, blank=False, null=False)
     tsymbol = models.CharField(max_length=100, blank=True, null=True)
-    highest_bid = models.FloatField()
-    lowest_ask = models.FloatField()
+    highest_bid = models.FloatField(blank=True, null=True)
+    lowest_ask = models.FloatField(blank=True, null=True)
     tokenid = models.CharField(max_length=500, default='')
     volume = models.FloatField()
     is_active = models.BooleanField(verbose_name='Активный', blank=False, null=False)
@@ -33,3 +33,17 @@ class UniswapOne(models.Model):
 
     class Meta:
         db_table = 'module_uniswap_one'
+
+
+class UniswapMarkets(models.Model):
+    market = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    token = models.CharField(max_length=256, blank=True, null=True)
+    tokenid = models.CharField(max_length=256, blank=True, null=True)
+    tsymbol = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(verbose_name='Активный', blank=False, null=False)
+
+    def __str__(self):
+        return self.token
+
+    class Meta:
+        db_table = 'uniswap_markets'
