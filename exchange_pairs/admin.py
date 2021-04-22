c854baf8214e15aa4951e656aaf60a1c672ad348
ares <server.ares@gmail.com>
2021-04-22 18:42:17 +0300

    2.3.1b fix frontend

--- exchange_pairs/admin.py
+++ exchange_pairs/admin.py
@@ -1,4 +1,9 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+
 from .models import *
 
 
@@ -21,9 +26,48 @@ class SettingsModulesAdmin(admin.ModelAdmin):
 
 @admin.register(TrustedPairs)
 class TrustedPairsAdmin(admin.ModelAdmin):
-    list_display = ('token', 'contract', 'tsymbol', 'decimals', 'is_active')
+    list_display = ('token', 'contract', 'tsymbol', 'decimals', 'is_active', 'token_actions',)
     list_filter = ('is_active',)
     search_fields = ('token', 'tsymbol', 'contract',)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(r'^(?P<token_id>.+)/uniswap/$', self.process_uniswap, kwargs=[], name='token-uniswap'),
+            # url(
+            #     r'^(?P<token_id>.+)/withdraw/$',
+            #     self.admin_site.admin_view(self.process_withdraw),
+            #     name='account-withdraw',
+            # ),
+        ]
+        return custom_urls + urls
+
+    def token_actions(self, obj):
+        # TODO: Render action buttons
+        return format_html(
+            '<a class="button" href="{}">Uniswap</a> ',
+            # '<a class="button" href="{}">Withdraw</a>',
+            # reverse('admin:account-deposit', args=[obj.pk]),
+            reverse('admin:token-uniswap', args=[obj.pk]),
+        )
+    token_actions.short_description = 'Token Actions'
+    token_actions.allow_tags = True
+
+    def process_uniswap(self, request, token_id, *args, **kwargs):
+        obj = TrustedPairs.objects.get(id=token_id)
+        try:
+            uniObj = Uniswap.objects.get(tokenid__icontains=obj.contract)
+            uniObj.tsymbol = obj.tsymbol
+            uniObj.is_active = True
+            uniObj.save()
+        except:
+            obj = Uniswap(tokenid=obj.contract, tsymbol=obj.tsymbol, exch_direction=obj.token, is_active=True)
+            obj.save()
+        return HttpResponseRedirect("/admin/exchange_pairs/trustedpairs/")
+        # TODO
+    def process_withdraw(self):
+        pass
+        # TODO
 
 
 @admin.register(WebsocketLog)
